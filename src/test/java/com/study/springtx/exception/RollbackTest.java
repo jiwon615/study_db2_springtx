1210b94e8a0c741d2acbc1c443671a4c70c13e4f
jiwon615 <seg615@naver.com>
2023-02-12 09:26:44 +0900

    comment: 주석 추가

--- src/test/java/com/study/springtx/exception/RollbackTest.java
+++ src/test/java/com/study/springtx/exception/RollbackTest.java
@@ -10,6 +10,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+/**
+ * 예외 발생시 스프링 트랜잭션 AOP는 예외의 종류에 따라 트랜잭션을 커밋하거나 롤백한다.
+ * 언체크 예외인 RuntimeException , Error 와 그 하위 예외가 발생하면 트랜잭션을 롤백한다. 체크 예외인 Exception 과 그 하위 예외가 발생하면 트랜잭션을 커밋한다.
+ * 물론 정상 응답(리턴)하면 트랜잭션을 커밋한다.
+ */
 @SpringBootTest
 public class RollbackTest {
 
