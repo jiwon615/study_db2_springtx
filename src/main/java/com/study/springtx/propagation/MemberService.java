d9a74570c029a0a28923dc816d8f430e7873b89c
jiwon615 <seg615@naver.com>
2023-02-13 22:59:20 +0900

    feat: 회원정보와 디비 로그 쌓기 별도의 트랜잭션으로 각 실행 테스트

---
+++ src/main/java/com/study/springtx/propagation/MemberService.java
@@ -0,0 +1,47 @@
+package com.study.springtx.propagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final LogRepository logRepository;
+
+    // 회원과 DB로그 함께 남김 (트랜잭션 처리 하지 않음)
+    public void joinV1(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        logRepository.save(logMessage);
+        log.info("== logRepository 호출 종료 ==");
+    }
+
+    // 위와 동일 기능 (다만, DB로그 저장시 예외 발생하면 예외를 복구; 트랜잭션 처리 하지 않음)
+    public void joinV2(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        try {
+            logRepository.save(logMessage);
+        } catch (RuntimeException e) {
+            log.info("log 저장에 실패했습니다. logMessage={}", logMessage.getMessage());
+            log.info("정상 흐름 변환");
+        }
+        log.info("== logRepository 호출 종료 ==");
+    }
+}
